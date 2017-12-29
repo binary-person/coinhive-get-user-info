6360cab78c626bff5e6af7adc5157f212ab71560
Simon Cheng <simoncheng559@gmail.com>
2017-12-29 20:59:37 +0000

    Accidently added node_modules

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "secretKey": "K8CXJI6x9H18mF7IJytVjezbWzAGLW9M",
+    "secretKey": "",
     "timeLength": "30000", // In miliseconds, and the longer the period, the better the accuracy
-    "keepAliveUrl": "https://coinhive-user-info-sc.now.sh"
+    "keepAliveUrl": ""
 };
